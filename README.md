af99701f5cd404f76e50f39c39f7efb020224cb0
Elbe <ashelbe08@gmail.com>
2019-01-10 23:33:25 -0500

    Added authentication

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Train-Scheduler
 Basic Train Schedule
+
+Train Scheduler
+
+Using HTML, CSS, Bootstrap, Javascript, JQuery, FireBase
+
+This project is a train scheduler application that incorporates Firebase to host arrival and departure data. The application will retrieve and manipulate this information with Moment.js.
+It will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+### Instructions
+
+1. The project allows the user to add in train details, that would be saved in Firebase Db.
+
+2. The next arrival time, and time left to next train are calculated and displayed.
+
+3. The user is also allowed to delete or edit the train details.
+
+4. The user will be authenticated using FirebaseUI Widget, using email and password.
+   Guests could use the email:guest@gmail.com, password as values to login.
+
+
+Pending (Extra Challenges)
+
+* Consider updating your "minutes to arrival" and "next train time" text once every minute. This is significantly more challenging; only attempt this if you've completed the actual activity and committed it somewhere on GitHub for safekeeping (and maybe create a second GitHub repo).
+
+
+
+
+
--- assets/javascript/train-scheduler.js
+++ assets/javascript/train-scheduler.js
@@ -9,74 +9,68 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-var trainRef=database.ref("/train");
+var trainRef = database.ref("/train");
 
 //Functions
-function addTrainData(trainSnapshot){
+function addTrainData(trainSnapshot) {
   var name = trainSnapshot.val().name;
   var destination = trainSnapshot.val().destination;
   var trainTime = trainSnapshot.val().time;
-  var frequency= trainSnapshot.val().frequency;
+  var frequency = trainSnapshot.val().frequency;
 
   //Calculate minutes away
-  var minutesAway=getMinutesAway(trainTime,frequency);
+  var minutesAway = getMinutesAway(trainTime, frequency);
 
   // Calculate next arrival time
-  var nextArrival=getArrivalTime(minutesAway);
+  var nextArrival = getArrivalTime(minutesAway);
   var key = trainSnapshot.key;
 
-  var $tr=$("<tr>");
-  //$tr.addClass("row");
-  $tr.attr("id",key)
-     .append(`<td>${name}</td>`)
-     .append(`<td>${destination}</td>`)
-     .append(`<td>${trainTime}</td>`)
-     .append(`<td>${frequency}</td>`)
-     .append(`<td>${nextArrival}</td>`) 
-     .append(`<td>${minutesAway}</td>`)
-     .append(`<td><a href='#' onclick="modifyTrain('${key}')"><i class="fas fa-pencil-alt"></i></a>&nbsp;&nbsp;<a href='#' onclick="deleteTrain('${key}')"><i class="fas fa-trash-alt"></i></a></td>`);
-     
-     
+  var $tr = $("<tr>");
+  $tr.attr("id", key)
+    .append(`<td>${name}</td>`)
+    .append(`<td>${destination}</td>`)
+    .append(`<td>${trainTime}</td>`)
+    .append(`<td>${frequency}</td>`)
+    .append(`<td>${nextArrival}</td>`)
+    .append(`<td>${minutesAway}</td>`)
+    .append(`<td><a href='#' onclick="modifyTrain('${key}')"><i class="fas fa-pencil-alt"></i></a>&nbsp;&nbsp;<a href='#' onclick="deleteTrain('${key}')"><i class="fas fa-trash-alt"></i></a></td>`);
+
   $("#train-info").append($tr);
 }
 
 
-function modifyTrain(key){
-  console.log("in modify" + key);
-
+function modifyTrain(key) {
   //Get data from DB and set in input
-  database.ref("/train/"+key).once("value").then(function(trainSnapshot){
+  database.ref("/train/" + key).once("value").then(function (trainSnapshot) {
     $("#trainName").val(trainSnapshot.val().name);
     $("#destination").val(trainSnapshot.val().destination);
     $("#trainTime").val(trainSnapshot.val().time);
     $("#frequency").val(trainSnapshot.val().frequency);
   });
-  $("#update-btn").attr("data-key",key);
+
+  $("#update-btn").attr("data-key", key);
   $("#submit-btn").hide();
   $("#update-btn").show();
-  
-   
 }
 
-function deleteTrain(key){
-  
-  var trainD = database.ref("/train/"+key);
+function deleteTrain(key) {
+  var trainD = database.ref("/train/" + key);
   trainD.remove();
 
   //Remove row from table
-  $('#'+key).remove();
+  $('#' + key).remove();
 }
 
-function getArrivalTime(tMinutesTillTrain){
+function getArrivalTime(tMinutesTillTrain) {
   // Next Train
   var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm a");
   //console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
   return nextTrain;
 }
 
-function getMinutesAway(firstTime, tFrequency){
+function getMinutesAway(firstTime, tFrequency) {
 
-  console.log(firstTime,tFrequency);
+  console.log(firstTime, tFrequency);
   // First Time (pushed back 1 year to make sure it comes before current time)
   var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
 
@@ -90,74 +84,71 @@ function getMinutesAway(firstTime, tFrequency){
   return tMinutesTillTrain;
 }
 
-var update = function() {
+// To set running clock on header
+var update = function () {
   document.getElementById("currentDay")
-  .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
 setInterval(update, 1000);
 
 
-function getCurrentTime(){
-  return moment().format("MMM Do Y  HH:mm:ss a");
-}
-
 //Event handlers
-$(document).ready(function(){
+$(document).ready(function () {
 
   $("#submit-btn").show();
   $("#update-btn").hide();
 
   // On submit 
-  $("#submit-btn").on("click",function(){
-    //event.preventDefault();
+  $("#submit-btn").on("click", function () {
+
     var trainData = {
-      name : $("#trainName").val().trim(),
-      destination : $("#destination").val().trim(),
-      time : $("#trainTime").val().trim(),
-      frequency : $("#frequency").val().trim()
+      name: $("#trainName").val().trim(),
+      destination: $("#destination").val().trim(),
+      time: $("#trainTime").val().trim(),
+      frequency: $("#frequency").val().trim()
     };
 
-    console.log(trainData);
+    //console.log(trainData);
 
     trainRef.push(trainData);
     document.getElementById("myForm").reset();
-   
-  });  
 
- // When Value Updated
- $("#update-btn").on("click",function(){
-  var trainData = {
-    name : $("#trainName").val().trim(),
-    destination : $("#destination").val().trim(),
-    time : $("#trainTime").val().trim(),
-    frequency : $("#frequency").val().trim()
-  };
+  });
 
-  var key = $(this).attr("data-key");
-  console.log("key" + key);
-  database.ref("/train/"+key).update(trainData);
+  // When Value Updated
+  $("#update-btn").on("click", function () {
+    var trainData = {
+      name: $("#trainName").val().trim(),
+      destination: $("#destination").val().trim(),
+      time: $("#trainTime").val().trim(),
+      frequency: $("#frequency").val().trim()
+    };
 
-  document.getElementById("myForm").reset();
+    var key = $(this).attr("data-key");
+    //console.log("key" + key);
+    database.ref("/train/" + key).update(trainData);
 
-  $("#submit-btn").show();
-  $("#update-btn").hide();
- });
+    document.getElementById("myForm").reset();
+
+    $("#submit-btn").show();
+    $("#update-btn").hide();
+  });
 
   //Listener for new train details added
-  trainRef.on("child_added",function(trainSnapshot){
+  trainRef.on("child_added", function (trainSnapshot) {
     addTrainData(trainSnapshot);
-  },function(errObject){
+  }, function (errObject) {
     console.log("Error Occurred : " + code);
   });
 
   //Listener for train modified
-  trainRef.on("child_changed",function(trainSnapshot){
+  trainRef.on("child_changed", function (trainSnapshot) {
 
     //Remove row from table
-    $('#'+trainSnapshot.key).remove();
+    $('#' + trainSnapshot.key).remove();
 
     addTrainData(trainSnapshot);
-  },function(errObject){
+  }, function (errObject) {
     console.log("Error Occurred : " + code);
   });
 });
